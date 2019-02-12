830f6f637d5fad9bfe0bc7ca51af7d1e9c5eaa74
Ricardo Castro <ricardocastrosis@gmail.com>
2019-02-12 17:20:59 -0200

    Projeto criado. Primeira versão do código.

---
+++ src/main/java/com/curso/acursospring/AcursoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.curso.acursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcursoSpringApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/curso/acursospring/AcursoSpringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.curso.acursospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcursoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
