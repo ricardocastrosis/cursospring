57f780c4382bbd1fb37353ae2e98623ce3c74966
Ricardo Castro <ricardocastrosis@gmail.com>
2019-02-12 17:57:57 -0200

    Teste classe Categoria

--- src/main/java/com/curso/acursospring/resources/CategoriaResource.java
+++ src/main/java/com/curso/acursospring/resources/CategoriaResource.java
@@ -5,6 +5,9 @@
  */
 package com.curso.acursospring.resources;
 
+import com.curso.acursospring.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
     
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST rodando!";
+    public List<Categoria> listar(){
+        
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        
+        return lista;
     }
     
 }
